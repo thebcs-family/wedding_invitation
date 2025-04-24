2467df3115b0b4839bf41e98819ff3e717a12d75
thebcs-family <thebcs.family@gmail.com>
2025-04-24 20:06:06 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -7,6 +7,10 @@ const nextConfig = {
   basePath: process.env.NODE_ENV === 'production' ? '/wedding_invitation' : '',
   assetPrefix: process.env.NODE_ENV === 'production' ? '/wedding_invitation/' : '',
   trailingSlash: true,
+  // Add this line to ignore ESLint errors during build
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 }
 
 module.exports = nextConfig
