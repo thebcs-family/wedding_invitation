866363909779efadf22bf4bedf877b7d2a293383
cecy07 <cecy@o.cnu.ac.kr>
2025-04-28 21:12:41 +0900

    Change to HTML

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import WeddingInvitation from './components/WeddingInvitation';
-
-export default function Home() {
-  return (
-    <main className="min-h-screen bg-gradient-to-b from-pink-50 to-white">
-      <WeddingInvitation />
-    </main>
-  );
-} 
