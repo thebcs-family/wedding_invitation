094241b06ef14e18255270188c61b671dc7261a8
GitHub Action <action@github.com>
2025-04-28 16:07:28 +0000

    Update secrets.js with injected values

---
+++ public/secrets.js
@@ -0,0 +1,10 @@
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyAGBR348j3Gy0aQToEtCs2O-ZmqspJzsAc",
+  authDomain: "invitationwed-47162.firebaseapp.com",
+  projectId: "invitationwed-47162",
+  storageBucket: "invitationwed-47162.firebasestorage.app",
+  messagingSenderId: "715035683043",
+  appId: "1:715035683043:web:0bb6d6ac831984097b3dd6",
+  measurementId: "G-HFJ8B2D533"
+};
+window.KAKAO_APP_KEY = "bb132f8bc221cfde1f8dd1389dca3c53";
