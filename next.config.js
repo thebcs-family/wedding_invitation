147bf5878cb66e8957ec3e8b1aea15f779555319
cecy07 <cecy@o.cnu.ac.kr>
2025-04-29 14:50:53 +0900

    Next js deployment

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true
+  },
+  // Add any other Next.js config options here
+}
+
+module.exports = nextConfig 
